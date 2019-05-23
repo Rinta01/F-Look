19cf65139adf8c0e9a195a7f899589262f093272
Rinta01 <rinat.ganbarov@gmail.com>
2019-05-23 23:00:37 +0300

    prodUri

--- src/utils/apolloConfig.js
+++ src/utils/apolloConfig.js
@@ -5,13 +5,14 @@ import { onError } from 'apollo-link-error';
 import { setContext } from 'apollo-link-context';
 import { ApolloLink } from 'apollo-link';
 
-const graphqlUri = 'http://localhost:4000/graphql';
+const prodBaseUri = 'https://f-back.herokuapp.com';
+const localBaseUri = 'http://localhost:4000';
+const graphqlUri = `${prodBaseUri}/graphql`;
 const httpLink = new HttpLink({
 	uri: graphqlUri,
 });
 const errorLink = onError(({ response, graphQLErrors, networkError }) => {
-	if (graphQLErrors)
-		graphQLErrors.map(({ message, path }) => console.log(`[GraphQL error]: Message: ${message}, Path: ${path}`));
+	if (graphQLErrors) graphQLErrors.map(({ message, path }) => console.log(`[GraphQL error]: Message: ${message}, Path: ${path}`));
 	if (networkError) {
 		console.log(`[Network error]: ${networkError}`);
 	}
